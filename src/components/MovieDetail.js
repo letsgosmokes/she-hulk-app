416c10331926daa9b88e2661588170c0b04493aa
letsgosmokes <saroy032@gmail.com>
2024-06-16 00:32:58 +0530

    Final changes (#10)
    * moved api calls to service
    
    * reset search term in details page
    
    * input css
    
    * optimise
    
    * change app name
    
    * added circular loader

--- src/components/MovieDetail.js
+++ src/components/MovieDetail.js
@@ -1,21 +1,31 @@
-import React, { useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import axios from 'axios';
 import { fallbackImgUrl } from '../utils/constants';
 import { handleImageError } from '../utils/utilFn';
 import Loader from './Loader';
+import { fetchMovieDetails } from '../service/movies';
+import { MovieContext } from '../context/MovieContext';
 
 function MovieDetail() {
     const { id } = useParams();
     const [movie, setMovie] = useState(null);
+    const { setSearchTerm } = useContext(MovieContext);
+
+    const getDetailsOfMovie = async () => {
+        try {
+            const movieDetails = await fetchMovieDetails(id);
+            if (movieDetails) {
+                setMovie(movieDetails); // append new data after scrolling
+            }
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    };
 
     useEffect(() => {
-        axios.get(`https://www.omdbapi.com/?apikey=b9bd48a6&i=${id}`)
-            .then(response => {
-                setMovie(response.data);
-            })
-            .catch(error => console.error('Error fetching data:', error));
-    }, [id]);
+        getDetailsOfMovie();
+        setSearchTerm("");
+    }, []);
 
     if (!movie) {
         return <Loader />;
@@ -26,14 +36,14 @@ function MovieDetail() {
             <div className='movie-poster'>
                 <img src={movie.Poster || fallbackImgUrl} alt={movie.Title} onError={handleImageError} />
             </div>
-            <h2>{movie.Title} ({movie.Year})</h2>
+            <h2 className='orange'>{movie.Title} ({movie.Year})</h2>
             <div className='movie-meta'>
-                <p><strong>Genre:</strong><br /> {movie.Genre}</p>
-                <p><strong>Running Time:</strong><br />  {movie.Runtime}</p>
-                <p><strong>Rating:</strong><br />  {movie.imdbRating}/10</p>
+                <p><span className='orange'><strong>Genre:</strong></span><br /> {movie.Genre}</p>
+                <p><span className='orange'><strong>Running Time:</strong></span><br />  {movie.Runtime}</p>
+                <p><span className='orange'><strong>Rating:</strong></span><br />  {movie.imdbRating}/10</p>
             </div>
-            <p><strong>Plot:</strong><br />  {movie.Plot}</p>
-            <p><strong>Actors:</strong><br /> {movie.Actors}</p>
+            <p><span className='orange'><strong>Plot:</strong></span><br />  {movie.Plot}</p>
+            <p><span className='orange'><strong>Actors:</strong></span><br /> {movie.Actors}</p>
         </div>
     );
 }
